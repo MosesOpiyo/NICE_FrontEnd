ba18e3b1ff44a15913b9d373e329ceb2a546bc82
MosesOpiyo <mosesopiyo54@gmail.com>
2023-08-02 10:59:32 +0300

    Coffee products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { CoffeeComponent } from './coffee/coffee.component';
 
 const routes: Routes = [
   {path:'',component:HomepageComponent},
   {path:'Signup',component:SignupComponent},
+  {path:'Coffee',component:CoffeeComponent}
 ];
 
 @NgModule({
---
+++ src/app/coffee-navbar/coffee-navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'
+import { LoginComponent } from '../login/login.component';
+import { AuthenticationService } from '../AuthService/authentication.service';
+
+@Component({
+  selector: 'app-coffee-navbar',
+  templateUrl: './coffee-navbar.component.html',
+  styleUrls: ['./coffee-navbar.component.css']
+})
+export class CoffeeNavbarComponent implements OnInit {
+  constructor(private dialog: MatDialog,private service:AuthenticationService){}
+  user:any | null = null;;
+
+  showLoginDialog(){
+    const dialogRef = this.dialog.open(LoginComponent,{
+      width: '25pc'
+    }); 
+  }
+  ngOnInit(): void {
+    this.service.getProfile().subscribe((res:any)=>{
+      this.user = res['user']
+    })
+  }
+
+}
