8b9a8074ccae0429bec83b91e53ffe9085ebbf67
MosesOpiyo <mosesopiyo54@gmail.com>
2023-08-04 15:14:38 +0300

    warehouse dashboard editing

--- src/app/warehousedashboard/components/myaccount/myaccount.component.ts
+++ src/app/warehousedashboard/components/myaccount/myaccount.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/AuthService/authentication.service';
 
 @Component({
   selector: 'app-myaccount',
   templateUrl: './myaccount.component.html',
   styleUrls: ['./myaccount.component.css']
 })
-export class MyaccountComponent {
+export class MyaccountComponent implements OnInit {
+  constructor(private service:AuthenticationService){}
+  user:any | null = null;;
+  ngOnInit(): void {
+    this.service.getProfile().subscribe((res:any)=>{
+      this.user = res['user']
+    })
+  }
 
 }
