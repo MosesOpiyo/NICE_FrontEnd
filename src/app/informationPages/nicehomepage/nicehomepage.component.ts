fc6434b766cc281058ee2bb73a0e07023d0c1327
r3t3p <92801240+PgGitau@users.noreply.github.com>
2023-08-21 18:17:39 +0300

    homepage edits

--- src/app/informationPages/nicehomepage/nicehomepage.component.ts
+++ src/app/informationPages/nicehomepage/nicehomepage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'
+import { LoginComponent } from 'src/app/login/login.component'; 
+import { AuthenticationService } from 'src/app/AuthService/authentication.service';
 
 @Component({
   selector: 'app-nicehomepage',
@@ -7,47 +10,207 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NicehomepageComponent implements OnInit {
 
-  farmerData = [ 
+  coffeeData = [ 
     {
       id: 1,
-      name: 'Riakiania Coffee Factory',
-      link: "riakiania"
+      imageUrl: '../../assets/img/packaging/pkg7.jpg',
+      sticker: true,
+      farmer: 'Riakiania Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star',
+      ],
+      productName: 'AA Coffee',
+      newPrice: '78',
+      oldPrice: '90',
+      discountPercentage: '7'
     },
 
     {
       id: 2,
-      name: 'Kariaini coffee factory',
-      link: "kariaini"
+      imageUrl: '../../assets/img/packaging/pkg8.jpg',
+      sticker: true,
+      farmer: 'Kiaragana Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star',
+        'bi bi-star',
+      ],
+      productName: 'AB Coffee',
+      newPrice: '80',
+      oldPrice: '90',
+      discountPercentage: '5'
     },
 
     {
       id: 3,
-      name: 'Gathambi coffee factory',
-      link: "gathambi"
+      imageUrl: '../../assets/img/coffee-04.jpg',
+      sticker: true,
+      farmer: 'Kaga Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+      ],
+      productName: 'PB Coffee',
+      newPrice: '70',
+      oldPrice: '100',
+      discountPercentage: '10'
     },
 
     {
       id: 4,
-      name: 'Kiaragana coffee factory',
-      link: "kiaragana"
+      imageUrl: '../../assets/img/packaging/pkg1.jpg',
+      sticker: true,
+      farmer: 'Kiambwe Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star',
+        'bi bi-star',
+      ],
+      productName: 'TT Coffee',
+      newPrice: '50',
+      oldPrice: '80',
+      discountPercentage: '10'
     },
 
     {
       id: 5,
-      name: 'Kiambwe Coffee Factory',
-      link: "kiambwe"
+      imageUrl: '../../assets/img/packaging/pkg10.jpg',
+      sticker: true,
+      farmer: 'Mitondo Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+      ],
+      productName: 'AA Coffee',
+      newPrice: '70',
+      oldPrice: '100',
+      discountPercentage: '15'
     },
 
     {
       id: 6,
-      name: 'Mitondo Coffee Factory',
-      link: "mitondo"
-    }
+      imageUrl: '../../assets/img/coffee-bag-02.jpg',
+      sticker: true,
+      farmer: 'Gathambi Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star',
+      ],
+      productName: 'T Coffee',
+      newPrice: '80',
+      oldPrice: '90',
+      discountPercentage: '7'
+    },
+
+    {
+      id: 7,
+      imageUrl: '../../assets/img/coffee-bag-03.jpg',
+      sticker: true,
+      farmer: 'Ihara Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star',
+        'bi bi-star',
+      ],
+      productName: 'AA Coffee',
+      newPrice: '70',
+      oldPrice: '90',
+      discountPercentage: '10'
+    },
+
+    {
+      id: 8,
+      imageUrl: '../../assets/img/packaging/pkg9.jpg',      
+      sticker: true,
+      farmer: 'Getuya Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star',
+      ],
+      productName: 'PB Coffee',
+      newPrice: '78',
+      oldPrice: '90',
+      discountPercentage: '7'
+    },
+
+    {
+      id: 9,
+      imageUrl: '../../assets/img/coffee-bag-01.jpg',
+      sticker: true,
+      farmer: 'Ikuni Coffee Factory',
+      rating:  [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+      ],
+      productName: 'AB Coffee',
+      newPrice: '80',
+      oldPrice: '90',
+      discountPercentage: '10'
+    },
+
+    {
+      id: 10,
+      imageUrl: '../../assets/img/coffee-bag-03.jpg',
+      sticker: true,
+      farmer: 'Zambezi Coffee Factory',
+      rating: [
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+        'bi bi-star-fill',
+      ],
+      productName: 'TT Coffee',
+      newPrice: '78',
+      oldPrice: '90',
+      discountPercentage: '7'
+    },
   ]
 
-  constructor () {}
-  ngOnInit(): void {
-    
+  myScriptElement: HTMLScriptElement;
+   constructor(private dialog: MatDialog,private service:AuthenticationService){
+      this.myScriptElement = document.createElement("script");
+      this.myScriptElement.src = "../../assets/js/main.js";
+      document.body.appendChild(this.myScriptElement);
+   }
+
+   user:any | null = null;
+
+   showLoginDialog(){
+    const dialogRef = this.dialog.open(LoginComponent,{
+      width: '25pc'
+    }); 
   }
+
+   ngOnInit(): void {
+    this.service.getProfile().subscribe((res:any)=>{
+      this.user = res['user']
+    })
+   }
 }
 
