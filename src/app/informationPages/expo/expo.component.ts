babe85ba5e2670f5e70725b72a092943ecbfadce
unknown <githinjipete21@gmail.com>
2023-10-22 22:58:37 +0300

    add expo page details, edit trip header and their corresponding styling

--- src/app/informationPages/expo/expo.component.ts
+++ src/app/informationPages/expo/expo.component.ts
@@ -5,10 +5,49 @@ import { LoginComponent } from 'src/app/login/login.component';
 import { AuthenticationService } from 'src/app/AuthService/authentication.service'; 
 import { SignUpComponent } from 'src/app/sign-up/sign-up.component';
 import { AuthService } from 'src/app/Auth/auth.service';
+import { interval, Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 // import { PureCounter } from '@srexi/purecounterjs';
 // const pure = new PureCounter();
 
+interface timeComponents {
+  secondsToDday: number;
+  minutesToDday: number;
+  hoursToDday: number;
+  daysToDday: number;
+}
 
+function calcDateDiff(endDay: Date = new Date(2023, 10, 9)): timeComponents {
+  const dDay = endDay.valueOf();
+
+  const milliSecondsInASecond = 1000;
+  const hoursInADay = 24;
+  const minutesInAnHour = 60;
+  const secondsInAMinute = 60;
+
+  const timeDifference = dDay - Date.now();
+
+  const daysToDday = Math.floor(
+    timeDifference /
+      (milliSecondsInASecond * minutesInAnHour * secondsInAMinute * hoursInADay)
+  );
+
+  const hoursToDday = Math.floor(
+    (timeDifference /
+      (milliSecondsInASecond * minutesInAnHour * secondsInAMinute)) %
+      hoursInADay
+  );
+
+  const minutesToDday = Math.floor(
+    (timeDifference / (milliSecondsInASecond * minutesInAnHour)) %
+      secondsInAMinute
+  );
+
+  const secondsToDday =
+    Math.floor(timeDifference / milliSecondsInASecond) % secondsInAMinute;
+
+  return { secondsToDday, minutesToDday, hoursToDday, daysToDday };
+}
 @Component({
   selector: 'app-expo',
   templateUrl: './expo.component.html',
@@ -21,7 +60,14 @@ export class ExpoComponent implements OnInit {
      this.myScriptElement = document.createElement("script");
      this.myScriptElement.src = "./assets/js/main.js";
      document.body.appendChild(this.myScriptElement);
+
+     this.timeLeft$ = interval(1000).pipe(
+      map(x => calcDateDiff()),
+      shareReplay(1)
+    );
   }
+
+  public timeLeft$: Observable<timeComponents>;
 
   user:any | null = null;
   isLoggedIn:any
