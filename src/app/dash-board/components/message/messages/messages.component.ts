f17739864a0792b43986280aea4d8f40343b5e37
MosesOpiyo <mosesopiyo54@gmail.com>
2023-12-01 17:28:14 +0300

    Changes

--- src/app/dash-board/components/message/messages/messages.component.ts
+++ src/app/dash-board/components/message/messages/messages.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ProductsService } from 'src/app/ProductsService/products.service';
 import { AuthenticationService } from 'src/app/AuthService/authentication.service';
 import { NotificationMsgComponent } from '../notification-msg/notification-msg.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -10,31 +9,20 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
-  constructor(private products:ProductsService,private service:AuthenticationService,private dialog:MatDialog){}
+  constructor(private service:AuthenticationService,private dialog:MatDialog){}
   user:any
-
   masterSelected = false;
   index: any;
+  unreaded: any[] = [];
 
 checkUncheckAll(evt: any) {
-  this.checklist.forEach((c) => c.isSelected = evt.target.checked)
+  this.user.notifications.forEach((c) => c.isSelected = evt.target.checked)
 }
 
 isAllSelected(evt: any, index: any) {
-    this.checklist[index].isSelected = evt.target.checked
-    this.masterSelected = this.checklist.every((item) => item.isSelected == true);
+    this.user.notifications[index].isSelected = evt.target.checked
+    this.masterSelected = this.user.notifications.every((item) => item.isSelected == true);
 }
-
-checklist = [
-  {id:1,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'},
-  {id:2,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'},
-  {id:3,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'},
-  {id:4,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'},
-  {id:5,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'},
-  {id:6,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'},
-  {id:7,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'},
-  {id:8,isSelected:false,notification:'Your coffee #1234RT4 has arrived at warehouse #1'}
-];
 
 showNotification() {
   this.dialog.open(NotificationMsgComponent)
@@ -44,6 +32,13 @@ showNotification() {
   ngOnInit(): void {
     this.service.getProfile().subscribe((res:any)=>{
       this.user = res['user']
+      for(let message of this.user.notifications){
+        if(message.seen == false){
+          this.unreaded.push(message)
+          console.log(this.unreaded)
+        }
+      }
     })
+    
   }
 }
