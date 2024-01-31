9e74fb33e6eb1781132560feb9b0b1b743b58fdb
r3t3p <92801240+PgGitau@users.noreply.github.com>
2024-01-31 20:03:39 +0300

    edit dashboard header and orderdetails component's order timeline

--- src/app/dash-board/components/orderdetails/orderdetails.component.ts
+++ src/app/dash-board/components/orderdetails/orderdetails.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orderdetails',
   templateUrl: './orderdetails.component.html',
   styleUrl: './orderdetails.component.css'
 })
-export class OrderdetailsComponent {
+export class OrderdetailsComponent implements OnInit {
 
+ value = [
+  {content: 'Ordered', date: '15/02/2024 10:30', status: 'P'},
+  {content: 'Processing', date: '15/02/2024 10:30', status: 'P'},
+  {content: 'Shipped'},
+  {content: 'Delivered'},
+ ]
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
